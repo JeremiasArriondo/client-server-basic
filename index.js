1e9acfbd71a3a6e29f8a72c9584cc7db9f0c2cef
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2021-12-29 18:19:20 -0300

    [main] Se agrega metodo map para obtener cada personaje, se utiliza join para separar cada value del objeto

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const main = async () => {
     let response = await axios.get('https://rickandmortyapi.com/api/character')
     //reestructuración mas profunda de la data
     let { data: {results} } = response;
-    let personajes = results.map((personaje) => {
-        return{
-            id: CharacterData.id,
-            name: CharacterData.name,
+    let personajes = results.map((character) => {
+        return {
+            id: character.id,
+            name: character.name,
+            status: character.status,
+            species: character.species,
         };
-    });
-    // console.log(personajes);
+        /*Agrego un map para retornar unicamente los valores */
+    })
+    .map((personaje) => Object.values(personaje).join(','))
+    .join('\n')
+    //Realizo un join con un salto de linea, a la vez que me convierte todo en un strig
+    console.log(personajes);
 };
 
 main();
