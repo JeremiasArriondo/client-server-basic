baf1ef2ae53b06f22e9aaab25bffadac7bd25e1e
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2021-12-29 18:30:11 -0300

    [main] Creación de un archivo csv con los datos obtenidos desde una API

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+//fs para trabajar con file system, este a su vez trabaja con promesas
+const fs = require('fs').promises;
+//path para generar una ruta
+const path = require('path');
 
 const main = async () => {
     let response = await axios.get('https://rickandmortyapi.com/api/character')
@@ -14,9 +18,16 @@ const main = async () => {
         /*Agrego un map para retornar unicamente los valores */
     })
     .map((personaje) => Object.values(personaje).join(','))
-    .join('\n')
+    .join('\n');
+    /**
+     * Creacion de un archivo csv
+     * Writefile recibe dos parametros:
+     * El primero es el path donde se va a escribir el archivo, incluyendo el nombre del archivo
+     * El segundo son los datos a almacenar en ese archivo
+     */
+    await fs.writeFile(path.join(__dirname, 'data.csv'), personajes);
     //Realizo un join con un salto de linea, a la vez que me convierte todo en un strig
-    console.log(personajes);
+    // console.log(personajes);
 };
 
 main();
